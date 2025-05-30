8dcdf60699309d6cc0265ed248722c596987e35b
AliSabah22 <aliasabah11@gmail.com>
2025-05-30 15:13:01 -0400

    Fix: Remove nested .git directory and properly track all files

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -3,11 +3,11 @@
 import React from 'react'
 import { MenuItem as MenuItemType } from '@/data/menu'
 
-interface MenuItemProps {
-  item: MenuItemType
-}
+// interface MenuItemProps {
+//   item: MenuItemType
+// }
 
-const MenuItem: React.FC<MenuItemProps> = ({ item }) => {
+const MenuItem: React.FC<> = ({ item }) => {
   return (
     <div className="flex justify-between items-start py-4 border-b border-gold/20 last:border-0">
       <div className="flex-1">
